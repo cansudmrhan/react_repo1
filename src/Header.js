546cbb781af084e5b6ebb6fb265470969a229880
Cansu Demirhan <cansudmrhan@gmail.com>
2022-07-03 22:26:13 +0300

    commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+
+const Header = ({ title }) => {
+
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: "Default Title"
+}
+
+export default Header;
